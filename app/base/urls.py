b991e4de11849877fbcde0b4612237240db9447a
Eric Burden <ewb.dev@gmail.com>
2020-04-02 17:24:57 -0500

    Add dtd_request urls

--- app/base/urls.py
+++ app/base/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("questgiver.urls")),
+    path("", include("dtd_request.urls")),
+    path("help/", include("questgiver.urls")),
     path("admin/", admin.site.urls),
 ]
