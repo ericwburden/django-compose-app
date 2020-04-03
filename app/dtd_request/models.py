522741d80c8a48bd55e3c36b2b8a03a6bb9ce87e
Eric Burden <ewb.dev@gmail.com>
2020-04-03 14:45:31 -0500

    Change models and update initial migration
    Domains are now a list of tuples in the models file, the Domain model is
    now a child table of Request instead of a lookup table, Request.type_of_need()
    returns a string based on the number of domains, Request.needs()
    returns the list of associated domains

--- app/dtd_request/models.py
+++ app/dtd_request/models.py
@@ -13,17 +13,32 @@ review_choices = (
     ("CLOSED", "Closed"),
 )
 
+domains = (
+    (1, "Shelter/Housing"),
+    (2, "Employment"),
+    (3, "Income"),
+    (4, "Food and Nutrition"),
+    (5, "Childcare"),
+    (6, "Children's Education"),
+    (7, "Adult Education"),
+    (8, "Healthcare"),
+    (9, "Life Skills"),
+    (10, "Family Relationships/Social Network"),
+    (11, "Transportation/Mobility"),
+    (12, "Community Involvement"),
+    (13, "Parenting Skills"),
+    (14, "Legal"),
+    (15, "Mental Health"),
+    (16, "Substance Abuse"),
+    (17, "Safety"),
+    (18, "Disability Services"),
+    (19, "Credit/Financial Management"),
+    (20, "Spirituality"),
+)
+
 
 def short_code():
     return str(uuid4())[:8]
-
-
-class Domain(models.Model):
-    value = models.IntegerField(unique=True)
-    label = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.label
 
 
 class Request(models.Model):
@@ -51,12 +66,6 @@ class Request(models.Model):
         verbose_name="Backup phone number",
         help_text="###-###-####",
     )
-    type_of_need = models.ForeignKey(
-        Domain,
-        on_delete=models.CASCADE,
-        verbose_name="What kind of help are you looking for?",
-        blank=False,
-    )
     add_info = models.TextField(
         verbose_name="Please provide any additional information here",
         help_text="Share anything else you'd like for us to know",
@@ -64,19 +73,44 @@ class Request(models.Model):
     confirmation_code = models.CharField(max_length=8, default=short_code)
 
     def __str__(self):
-        return f'{self.contact}:{self.type_of_need} ({self.id})'
+        return f"{self.contact}:{self.created_at.date()} ({self.id})"
 
-    def save(self, response=None, *args, **kwargs):
+    def save(self, *args, **kwargs):
         super(Request, self).save(*args, **kwargs)
-        if not response:
+        if not self.response_set.exists():
             response = Response(request=self, status="PENDING")
-        response.save()
+            response.save()
 
     def last_response(self):
         return Response.objects.filter(request=self.id).order_by("-created_at").first()
 
     def status(self):
         return self.last_response().status if self.last_response() else "PENDING"
+
+    def type_of_need(self):
+        need_count = self.domain_set.count()
+        if not need_count:
+            return 'None Listed'
+        if need_count == 1:
+            return str(self.domain_set.first())
+        if need_count > 1:
+            return 'Multiple'
+
+    def needs(self):
+        return self.domain_set.all()
+
+class Domain(models.Model):
+    request = models.ForeignKey(Request, on_delete=models.CASCADE)
+    domain = models.IntegerField(
+        choices=domains,
+        verbose_name="What kind of help are you looking for?",
+    )
+
+    def __str__(self):
+        return next(i[-1] for i in domains if i[0] == self.domain)
+
+    def label(self):
+        return self.__str__()
 
 
 class Response(models.Model):
