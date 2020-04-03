24a68962e8c84fcb665678fb7ec124edc6339770
Eric Burden <ewb.dev@gmail.com>
2020-04-03 14:47:12 -0500

    Change admin to combine objects
    Response and Domain are now inline forms in the Request admin page
    instead of separate admin forms

--- app/dtd_request/admin.py
+++ app/dtd_request/admin.py
@@ -1,18 +1,27 @@
 import pytz
 from django.contrib import admin
 from django.utils import timezone
-from .models import Request, Response
+from .models import Request, Response, Domain
 
+
+class ResponseInline(admin.TabularInline):
+    model = Response
+    extra = 0
+    ordering = ("-created_at",)
+
+
+class DomainInline(admin.StackedInline):
+    model = Domain
+    extra = 0
 
 class RequestAdmin(admin.ModelAdmin):
     timezone.activate(pytz.timezone("America/Chicago"))
-    list_display = ("created_at", "contact", "primary_phone", "type_of_need", "status")
-
-
-class ResponseAdmin(admin.ModelAdmin):
-    timezone.activate(pytz.timezone("America/Chicago"))
-    list_display = ("request", "status", "created_at", "created_by")
+    list_display = ("created_at", "contact", "primary_phone", "status")
+    inlines = [
+        ResponseInline,
+        DomainInline
+    ]
 
 
 admin.site.register(Request, RequestAdmin)
-admin.site.register(Response, ResponseAdmin)
+
