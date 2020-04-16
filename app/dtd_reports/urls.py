04fcb851c52f7565becedec9f769861714e010d7
Eric Burden <ewb.dev@gmail.com>
2020-04-16 09:36:29 -0500

    Add report for total incoming requests

--- app/dtd_reports/urls.py
+++ app/dtd_reports/urls.py
@@ -4,6 +4,13 @@ from django.urls import path
 app_name = "dtd_reports"
 
 urlpatterns = [
+    # Template View URLs ---------------------------------------------------------------
+    path(
+        "total-calls-requests/report",
+        views.TotalCallsRequestsReport.as_view(),
+        name="total-calls-requests-report",
+    ),
+    # Data View URLs -------------------------------------------------------------------
     path(
         "call-type-report", views.call_type_report_data, name="call-type-report-data",
     ),
@@ -21,5 +28,10 @@ urlpatterns = [
         "request-domain-report",
         views.request_domain_report_data,
         name="request-domain-report-data",
+    ),
+    path(
+        "total-calls-requests-report",
+        views.total_calls_requests_report_data,
+        name="total-calls-requests-report-data",
     ),
 ]
--- app/dtd_reports/views.py
+++ app/dtd_reports/views.py
@@ -1,14 +1,20 @@
 import pytz
 
 from dtd_calls.models import Call
-from dtd_request.models import Domain, domains
+from dtd_request.models import Request, Domain, domains
 from datetime import timedelta
 
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Count, ExpressionWrapper, DurationField, F, Sum
 from django.db.models.functions import TruncDate
 from django.http import JsonResponse
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 from django.utils import timezone
+
+# =======================================================================================
+# region Data Views --------------------------------------------------------------------
+# =======================================================================================
 
 
 def call_type_report_data(request):
@@ -71,7 +77,7 @@ def call_duration_report_data(request):
     for entry in queryset:
         date_string = entry["date"].strftime("%m/%d/%Y")
         minutes = round(entry["total_duration"].seconds // 60)
-        
+
         if date_string not in labels:
             labels.append(date_string)
             if len(labels) > len(incoming_calls) + 1:
@@ -119,3 +125,29 @@ def request_domain_report_data(request):
     counts = [entry["n"] for entry in queryset]
 
     return JsonResponse(data={"labels": labels, "counts": counts})
+
+
+def total_calls_requests_report_data(request):
+    incoming_calls = Call.objects.filter(call_type="Incoming").count()
+    online_intakes = Request.objects.count()
+
+    labels = ["Incoming Calls", "Online Intakes"]
+    counts = [incoming_calls, online_intakes]
+
+    return JsonResponse(data={"labels": labels, "counts": counts})
+
+
+# =======================================================================================
+# endregion ----------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------
+# region Template Views ----------------------------------------------------------------
+# =======================================================================================
+
+
+class TotalCallsRequestsReport(LoginRequiredMixin, TemplateView):
+    template_name = "dtd_reports/total-calls-requests-report.html"
+
+
+# =======================================================================================
+# endregion ----------------------------------------------------------------------------
+# =======================================================================================
