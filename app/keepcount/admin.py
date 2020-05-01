3507f4e2c8fe37b5ffa6a5789a802ef654ce62fa
Eric Burden <ewb.dev@gmail.com>
2020-05-01 16:48:26 -0500

    Add Counter to admin

--- app/keepcount/admin.py
+++ app/keepcount/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Counter
 
-# Register your models here.
+class CounterAdmin(admin.ModelAdmin):
+    list_display = ("counter_name", "created_at", "value", "max_value", "total")
+
+admin.site.register(Counter, CounterAdmin)
