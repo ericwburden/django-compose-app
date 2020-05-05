e4109546f65d9de8150e238d2b3170b0bd9f9b0e
Eric Burden <ewb.dev@gmail.com>
2020-05-04 22:49:00 -0500

    Add CounterPosition inline to admin

--- app/keepcount/admin.py
+++ app/keepcount/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from .models import Counter
+from .models import Counter, CounterPosition
+
+
+class CounterPositionAdmin(admin.StackedInline):
+    model = CounterPosition
 
 
 class CounterAdmin(admin.ModelAdmin):
     list_display = ("counter_name", "created_at", "value", "max_value", "total")
+    inlines = (CounterPositionAdmin, )
 
 
 admin.site.register(Counter, CounterAdmin)
